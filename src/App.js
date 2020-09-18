2cba94ab53b9654cd509d87aafa4eb61478623ac
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-09-17 21:58:04 -0500

    Drag and drop image file and preview

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import './styles.css';
 
 function App() {
-  return <h1>Hello</h1>;
+  const [image, setImage] = useState({});
+
+  const handleDragEnter = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+
+    let files = e.dataTransfer.files[0];
+    handleFile(files);
+  };
+
+  const handleFile = (file) => {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onloadend = () => {
+      setImage({ src: reader.result });
+    };
+  };
+
+  return (
+    <div
+      onDragEnter={handleDragEnter}
+      onDragLeave={handleDragLeave}
+      onDragOver={handleDragOver}
+      onDrop={handleDrop}
+    >
+      <span>Texto</span>
+      {image && <img src={image.src}></img>}
+    </div>
+  );
 }
 
 export default App;
