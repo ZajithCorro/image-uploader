42de0638e62cd02f369f88520c1eaa39783f1986
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-09-28 21:37:26 -0500

    Global state in the app

--- src/Uploader.js
+++ src/Uploader.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './components/Card';
 
 import './globalStyles.css';
 
 export default function Uploader() {
+  const [image, setImage] = useState(undefined);
+
   return (
     <main>
-      <Card />
+      <Card setImage={setImage} />
     </main>
   );
 }
